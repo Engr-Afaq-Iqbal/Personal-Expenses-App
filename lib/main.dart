062b471283aad30daf4f862d00d89c0db6eb6deb
AFAQ IQBAL <engr.afaqiqbal@gmail.com>
2022-08-31 21:49:00 +0500

    ShowModelBottomSheet added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/material.dart';
-import './widgets/user_transactions.dart';
+import 'package:personal_expenses_app/widgets/new_transactions.dart';
+import 'package:personal_expenses_app/widgets/transaction_list.dart';
+import 'models/transaction.dart';
 
 void main() {
   runApp(PE());
 }
 
-class PE extends StatelessWidget {
+class PE extends StatefulWidget {
+  @override
+  _PEState createState() => _PEState();
+}
+
+class _PEState extends State<PE> {
+  String _selectedItems = '';
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  final List<Transaction> transaction = [
+    Transaction(
+      id: 't1',
+      title: 'Shoes',
+      amount: 50.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Books',
+      amount: 20.1,
+      date: DateTime.now(),
+    )
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+    );
+    setState(() {
+      transaction.add(newTx);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,10 +49,16 @@ class PE extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text('Personal Expenses App'),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+            )
+          ],
         ),
-        body: Column(
+        body: ListView(
             //mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Container(
                 width: double.infinity,
@@ -25,8 +67,28 @@ class PE extends StatelessWidget {
                   child: Text('Charts'),
                 ),
               ),
-              UserTransactions(),
+              TransactionList(transaction),
             ]),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            showBottomSheet(
+              context: context,
+              builder: (context) {
+                return ListView(
+                  children: [
+                    TextField(),
+                    GestureDetector(
+                      child: NewTransactions(_addNewTransaction),
+                      behavior: HitTestBehavior.opaque,
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
